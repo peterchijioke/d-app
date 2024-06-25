8c06aa7fbe2be5e4f312cb03439ae88443e5c992
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-06-25 05:09:01 +0100

    update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 //import { ReactComponent as MobileMenu } from '../../icons/MobileMenu.svg'
 //import { ReactComponent as Close } from '../../icons/Close.svg'
-import { ReactComponent as Logo } from '../../icons/Logo.svg'
+import { ReactComponent as Logo } from "../../icons/Logo.svg";
 
-import header1 from '../../image/header1.png'
-import header2 from '../../image/header2.png'
+import header1 from "../../image/header1.png";
+import header2 from "../../image/header2.png";
+import "./navbar.css";
 
-import './navbar.css'
+import { ethers } from "ethers";
+let address, signer, provider;
+
 const Navbar = () => {
+  const [isConnected, setIsConnected] = useState(0);
   //   const [Mobile, setMobile] = useState(false)
   //   useEffect(() => {
   //     WindowChange()
@@ -25,27 +29,46 @@ const Navbar = () => {
   //   }
 
   //   window.addEventListener('resize', WindowChange)
+  const setAddress = async (ethadd) => {
+    address = ethadd;
+    if (address !== null) {
+      setIsConnected(!isConnected);
+    }
+    console.log(address);
+  };
+  const handleMint = () => {
+    if (!isConnected) {
+      connectWallet();
+    } else mintNFT();
+  };
 
-  const handleMint = () => {}
+  const mintNFT = () => {};
+  const connectWallet = async () => {
+    provider = new ethers.BrowserProvider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+    setAddress(await signer.getAddress());
+  };
+
   const handleAbout = () => {
-    var scroll = document.getElementsByClassName('aboutBC')
-    window.scroll({ behavior: 'smooth', top: scroll[0].offsetTop })
-  }
+    var scroll = document.getElementsByClassName("aboutBC");
+    window.scroll({ behavior: "smooth", top: scroll[0].offsetTop });
+  };
   const handleRoadmap = () => {
-    var scroll = document.getElementsByClassName('roadmapBC')
-    window.scroll({ behavior: 'smooth', top: scroll[0].offsetTop - 20 })
-  }
+    var scroll = document.getElementsByClassName("roadmapBC");
+    window.scroll({ behavior: "smooth", top: scroll[0].offsetTop - 20 });
+  };
   const handleTeam = () => {
-    var scroll = document.getElementsByClassName('teamBackGround')
-    window.scroll({ behavior: 'smooth', top: scroll[0].offsetTop - 20 })
-  }
+    var scroll = document.getElementsByClassName("teamBackGround");
+    window.scroll({ behavior: "smooth", top: scroll[0].offsetTop - 20 });
+  };
   const handleFaq = () => {
-    var scroll = document.getElementsByClassName('faqScroll')
-    window.scroll({ behavior: 'smooth', top: scroll[0].offsetTop + 20 })
-  }
+    var scroll = document.getElementsByClassName("faqScroll");
+    window.scroll({ behavior: "smooth", top: scroll[0].offsetTop + 20 });
+  };
 
   return (
-    <div className='navbar'>
+    <div className="navbar">
       {/* <div className='navbarMobile'>
         <div className='navbarCenterIcon'>
           <div className='navbarMobileTopRight '>MobileLeftTitle</div>
@@ -71,52 +94,56 @@ const Navbar = () => {
         </div>
       </div>*/}
 
-      <div className='navbarContainer SlideRightAnimation'>
-        <div className='navbarLeft'></div>
-        <div className='navbarCenter'>
-          <div className='navbarCenterTop'>
+      <div className="navbarContainer SlideRightAnimation">
+        <div className="navbarLeft"></div>
+        <div className="navbarCenter">
+          <div className="navbarCenterTop">
             <Logo />
           </div>
-          <div className='navbarCenterBottom'>
-            <div className='navbarCenterItem' onClick={handleMint}>
+          <div className="navbarCenterBottom">
+            <div className="navbarCenterItem" onClick={handleMint}>
               Mint
             </div>
-            <div className='navbarCenterItem' onClick={handleAbout}>
+            <div className="navbarCenterItem" onClick={handleAbout}>
               About Binaryville
             </div>
-            <div className='navbarCenterItem' onClick={handleRoadmap}>
+            <div className="navbarCenterItem" onClick={handleRoadmap}>
               Roadmap
             </div>
-            <div className='navbarCenterItem' onClick={handleTeam}>
+            <div className="navbarCenterItem" onClick={handleTeam}>
               Team
             </div>
-            <div className='navbarCenterItem' onClick={handleFaq}>
+            <div className="navbarCenterItem" onClick={handleFaq}>
               FAQ
             </div>
           </div>
         </div>
-        <div className='navbarRight'></div>
+        <div className="navbarRight"></div>
       </div>
 
-      <div className='navbarContainer SlideRightAnimation'>
-        <div className='navbarLeft'>
-          <img src={header1} alt='' className='navbarBoxImage' />
+      <div className="navbarContainer SlideRightAnimation">
+        <div className="navbarLeft">
+          <img src={header1} alt="" className="navbarBoxImage" />
         </div>
-        <div className='navbarCenter'>
-          <div className='navbarBox'>
-            <div className='navbarBoxTitle'>
-              <span className='textHighlight'>Welcome</span> to Binaryville
+        <div className="navbarCenter">
+          <div className="navbarBox">
+            <div className="navbarBoxTitle">
+              <span className="textHighlight">Welcome</span> to Binaryville
             </div>
-            <div className='navbarBoxSubTitle'>a collection of 5,000 unique NFTs</div>
-            <div className='navbarBoxButton'>MINT NOW</div>
+            <div className="navbarBoxSubTitle">
+              a collection of 5,000 unique NFTs
+            </div>
+            <div onClick={handleMint} className="navbarBoxButton">
+              {isConnected ? " MINT NOW" : "CONNECT WALLET"}
+            </div>
           </div>
         </div>
-        <div className='navbarRight'>
-          <img src={header2} alt='' className='navbarBoxImage' />
+        <div className="navbarRight">
+          <img src={header2} alt="" className="navbarBoxImage" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
