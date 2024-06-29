a2c25983a08681757683b7ac558e528fb67e3117
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-06-29 11:39:13 +0100

    fix: update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,55 +1,57 @@
-import React from 'react'
+import React from "react";
 
-import { ReactComponent as DiscordSVG } from '../../icons/Discord.svg'
-import { ReactComponent as TwitterSVG } from '../../icons/Twitter.svg'
-import { ReactComponent as InstagramVG } from '../../icons/Instagram.svg'
+import { ReactComponent as DiscordSVG } from "../../icons/Discord.svg";
+import { ReactComponent as TwitterSVG } from "../../icons/Twitter.svg";
+import { ReactComponent as InstagramVG } from "../../icons/Instagram.svg";
 
-import { ReactComponent as OpenSeaSVG } from '../../icons/OpenSea.svg'
+import { ReactComponent as OpenSeaSVG } from "../../icons/OpenSea.svg";
 
-import './footer.css'
+import "./footer.css";
 const Footer = () => {
   const handleDiscord = () => {
-    window.open('')
-  }
+    window.open("");
+  };
 
   const handleTwitter = () => {
-    window.open('')
-  }
+    window.open("");
+  };
 
   const handleInstagram = () => {
-    window.open('')
-  }
+    window.open("");
+  };
   return (
-    <div className='footerBC componentFirst'>
-      <div className='footerMain componentSecond'>
-        <div className='roadmap'>
-          <div className='footerIcon'>
+    <div className="footerBC componentFirst">
+      <div className="footerMain componentSecond">
+        <div className="roadmap">
+          <div className="footerIcon">
             <OpenSeaSVG />
           </div>
-          <div className='footerIconText'>Open sea</div>
-          <div className='footerName'>Binaryville</div>
-          <div className='footerName'>About Binaryville</div>
+          <div className="footerIconText">Open sea</div>
+          <div className="footerName">Binaryville</div>
+          <div className="footerName">About Binaryville</div>
 
-          <div className='footerJoin'>Join us</div>
-          <div className='footerContainer'>
-            <div className='footerLink' onClick={handleDiscord}>
-              <DiscordSVG className='footerIcon' />
-              <div className='footerLinkText'>Discord</div>
+          <div className="footerJoin">Join us</div>
+          <div className="footerContainer">
+            <div className="footerLink" onClick={handleDiscord}>
+              <DiscordSVG className="footerIcon" />
+              <div className="footerLinkText">Discord</div>
             </div>
-            <div className='footerLink' onClick={handleTwitter}>
-              <TwitterSVG className='footerIcon' />
-              <div className='footerLinkText'>Twitter</div>
+            <div className="footerLink" onClick={handleTwitter}>
+              <TwitterSVG className="footerIcon" />
+              <div className="footerLinkText">Twitter</div>
             </div>
-            <div className='footerLink' onClick={handleInstagram}>
-              <InstagramVG className='footerIcon' />
-              <div className='footerLinkText'>Instagram</div>
+            <div className="footerLink" onClick={handleInstagram}>
+              <InstagramVG className="footerIcon" />
+              <div className="footerLinkText">Instagram</div>
             </div>
           </div>
-          <div className='footerBox' />
+          <div className="footerBox">
+            <span> © {new Date().getFullYear()}</span>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
